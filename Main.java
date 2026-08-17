9fab33b2d47c6777f06029eff28eefa43bf7eb3e
pgadekar6307-blip <pgadekar6307@gmail.com>
2026-08-17 12:01:20 +0530

    Made the first pokemon

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
-public class Main{
 
+public class Main{
+    public class Constants{
     static final int NUM_CREATURE = 9;
     static final int MOVES_TOTAL  = 8;
     static final int MOVES_BATTLE = 4;
@@ -33,6 +34,7 @@ public class Main{
     /*ROC*/  { 10,  20,  10,  10,  10,  10,  10,  10,   5 },
     /*FIG*/  { 20,  10,  10,  10,  10,   5,  20,  20,  10 }
     }; 
+}
 
     enum Phase {IDLE, BATTLE, OVER}
     enum Status { NONE, BURN, PARALYSIS, SLEEP, POISON }
@@ -88,5 +90,39 @@ public class Main{
         }
     }
 
-    
+    static class Fighter {
+        String name;
+        int creatureId, type;
+        int hp, maxHP;
+        int attack, spAtk, defence, spDef, speed;
+        Status status = Status.NONE;
+        int sleepTurns = 0;
+        Move[] moves = new Move[Constants.MOVES_BATTLE];
+    }
+
+    static class Party {
+        Fighter[] slots = new Fighter[Constants.PARTY_SIZE];
+        int active = 0;
+    }
+
+    static class CreatureData {
+        private static MoveBlueprint mv(String name, int power, int type, int accuracy, String cat, String eff){
+            return new MoveBlueprint(name, power, type, accuracy, cat, eff);
+        }
+
+        static final Template[] TEMPLATES = {
+
+            new Template("Emberfox", Constants.T_FIRE, 480, 90, 85, 60, 65, 95,
+            new MoveBlueprint[]{
+                mv("Ember",         40, Constants.T_FIRE, 100, "special", "burn"),
+                mv("Incenerate",    60, Constants.T_FIRE, 90, "special", null),
+                mv("Flamethrower",  90, Constants.T_FIRE, 85, "special", "burn"),
+                mv("Fire Blast",    120, Constants.T_FIRE, 70, "special", "burn"),
+                mv("Scratch",       40, Constants.T_NORMAL, 100, "physical", null),
+                mv("Quick Slash",   65, Constants.T_NORMAL, 100, "physical", null),
+                mv("Bite",          60, Constants.T_DARK, 100, "physical", null),
+                mv("Inferno",       140, Constants.T_FIRE, 50, "special", "burn"),
+            }),
+        };
+    }
 }
